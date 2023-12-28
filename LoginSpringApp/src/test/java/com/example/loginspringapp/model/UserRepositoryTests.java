8716b8f4ef2dd9e5c5b34e818ef19e7d5b2438b4
vladproduction <vladbogdantsev@gmail.com>
2023-12-28 15:08:35 +0200

    LoginSpringApp add

---
+++ LoginSpringApp/src/test/java/com/example/loginspringapp/model/UserRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.example.loginspringapp.model;
+
+
+import com.example.loginspringapp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("vlad@gmail.com");
+        user.setPassword("123");
+        user.setFirstName("Vlad");
+        user.setLastName("Bogdantsev");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+
+    }
+}

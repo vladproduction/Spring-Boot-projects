f594cdd4ab4161791e224cfbf839bd753f4b8612
vladproduction <vladbogdantsev@gmail.com>
2024-02-06 19:26:09 +0200

    Spring-MVC-HelloWorld add

---
+++ Spring-MVC-HelloWorld/src/main/java/com/vladproduction/springmvchelloworld/SpringMvcHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.vladproduction.springmvchelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcHelloWorldApplication.class, args);
+    }
+
+}
---
+++ Spring-MVC-HelloWorld/src/test/java/com/vladproduction/springmvchelloworld/SpringMvcHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladproduction.springmvchelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

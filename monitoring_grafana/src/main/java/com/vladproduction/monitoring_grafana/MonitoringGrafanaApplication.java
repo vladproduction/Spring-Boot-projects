18a82a0999d6ff880431b073d960e80a1d56884b
vladproduction <vladbogdantsev@gmail.com>
2025-03-23 16:00:58 +0200

    demo example to monitor app with prometheus/grafana 2

---
+++ monitoring_grafana/src/main/java/com/vladproduction/monitoring_grafana/MonitoringGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.vladproduction.monitoring_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringGrafanaApplication.class, args);
+    }
+
+}
